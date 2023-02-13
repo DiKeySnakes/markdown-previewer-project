23d19537af806166a4f4d41032ef14edef5700f2
DiKeySnakes <dikeysnakes@gmail.com>
2023-02-13 18:09:35 +0200

    Add fontawesome npm modules and @uiw/react-markdown-preview npm module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div id='logo'>
+        <img src={logo} className='App-logo' alt='logo' />
+      </div>
+      <div id='footer'>
+        <footer>
+          <p>
+            Created by DiKeySnakes for
+            <a
+              href='https://www.freecodecamp.org/'
+              target='_blank'
+              rel='noopener noreferrer'>
+              {' '}
+              freeCodeCamp <FontAwesomeIcon icon={brands('free-code-camp')} />
+            </a>{' '}
+            curriculum
+          </p>
+          <p>
+            Copyright © DiKeySnakes 2023
+            <a
+              href='https://github.com/DiKeySnakes/markdown-previewer-project'
+              target='_blank'
+              rel='noopener noreferrer'>
+              {' '}
+              <FontAwesomeIcon id='githubIcon' icon={brands('github')} />
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
